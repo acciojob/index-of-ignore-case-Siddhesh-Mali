24b428f78ef2aaee4ba5c013b0b9f687dc44d2a5
Siddhesh-Mali <129844841+Siddhesh-Mali@users.noreply.github.com>
2024-03-05 00:55:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 function indexOfIgnoreCase(s1, s2) {
-  // write your code here
-}
+    // write your code here
+    let str = String(s1).toLowerCase();
+    let substr = String(s2).toLowerCase();
+    let strN = str.length;
+    let substrN = substr.length;
+
+    // console.log(str,strN,substr,substrN)
+
+    for(let i=0; i<strN; i++){
+        // console.log(str.substring(i,i+substrN))
+        if(str.substring(i,i+substrN) == substr){
+            return i;
+        }
+    }
+    return -1;
+  }
 
 // Please do not change the code below
 const s1 = prompt("Enter s1:");
